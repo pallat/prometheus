1c833e0fc29143d608c4d00fb390096e9fb9fd81
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2020-03-03 18:12:31 +0700

    update yml

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 docker run \
     -p 9090:9090 \
-    -v /Users/pallat/go/src/gitlab.com/pallat/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v /Users/pallat/go/src/github.com/pallat/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
     prom/prometheus
